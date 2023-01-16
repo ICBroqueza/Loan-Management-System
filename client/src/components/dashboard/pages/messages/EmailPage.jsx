3d3f23f1542b94a6e50e04cdea109c2d46a1e352
IC <icbroqueza13@gmail.com>
2023-01-16 20:31:10 +0800

    feat: init emailjs

--- client/src/components/dashboard/pages/messages/EmailPage.jsx
+++ client/src/components/dashboard/pages/messages/EmailPage.jsx
@@ -4,7 +4,7 @@ import GetBorrowers from './GetBorrowers';
 
 export default function EmailPage() {
   return (
-    <div className='flex'>
+    <div className='flex border-l-2'>
       <Sidebar />
 
       <div className='w-full px-8 pt-6 pb-8 mb-4 bg-white rounded'>
