fcd5a34fa3af8959ebe90d42eabfeafa8caecc4f
IC <icbroqueza13@gmail.com>
2023-01-17 22:50:40 +0800

    fix: landing page > pricing section styles

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -382,7 +382,7 @@ const Landing = () => {
               Designed for business teams like yours
             </h2>
             <p class='mb-5 font-light text-gray-500 sm:text-xl '>
-              Here at Landwind we focus on markets where technology, innovation,
+              Here at Maogma we focus on markets where technology, financing,
               and capital can unlock long-term value and drive economic growth.
             </p>
           </div>
@@ -430,23 +430,7 @@ const Landing = () => {
                   </svg>
                   <span>No setup, or hidden fees</span>
                 </li>
-                <li class='flex items-center space-x-3'>
-                  <svg
-                    class='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
-                    fill='currentColor'
-                    viewBox='0 0 20 20'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      fill-rule='evenodd'
-                      d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
-                      clip-rule='evenodd'
-                    ></path>
-                  </svg>
-                  <span>
-                    Team size: <span class='font-semibold'>1 developer</span>
-                  </span>
-                </li>
+
                 <li class='flex items-center space-x-3'>
                   <svg
                     class='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
@@ -545,23 +529,6 @@ const Landing = () => {
                     ></path>
                   </svg>
                   <span>
-                    Team size: <span class='font-semibold'>10 developer</span>
-                  </span>
-                </li>
-                <li class='flex items-center space-x-3'>
-                  <svg
-                    class='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
-                    fill='currentColor'
-                    viewBox='0 0 20 20'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      fill-rule='evenodd'
-                      d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
-                      clip-rule='evenodd'
-                    ></path>
-                  </svg>
-                  <span>
                     Premium support:{' '}
                     <span class='font-semibold'>24 months</span>
                   </span>
@@ -592,11 +559,11 @@ const Landing = () => {
               </a>
             </div>
 
-            {/* PRICE 1 */}
+            {/* PRICE 3 */}
             <div className='flex flex-col max-w-lg p-6 mx-auto text-center text-gray-900 bg-white border border-gray-200 rounded-lg shadow-md '>
-              <h3 class='mb-4 text-2xl font-semibold'>Starter</h3>
+              <h3 class='mb-4 text-2xl font-semibold'>Enterprise</h3>
               <p class='font-light text-gray-500 sm:text-lg dark:text-gray-400'>
-                Best option for personal use & for your next project.
+                Best for large scale users and extended redistribution rights.
               </p>
               <div class='flex items-baseline justify-center my-8'>
                 <span class='mr-2 text-5xl font-extrabold'>₱27.4k</span>
@@ -632,23 +599,6 @@ const Landing = () => {
                     ></path>
                   </svg>
                   <span>No setup, or hidden fees</span>
-                </li>
-                <li class='flex items-center space-x-3'>
-                  <svg
-                    class='flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400'
-                    fill='currentColor'
-                    viewBox='0 0 20 20'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      fill-rule='evenodd'
-                      d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
-                      clip-rule='evenodd'
-                    ></path>
-                  </svg>
-                  <span>
-                    Team size: <span class='font-semibold'>100+ developer</span>
-                  </span>
                 </li>
                 <li class='flex items-center space-x-3'>
                   <svg
