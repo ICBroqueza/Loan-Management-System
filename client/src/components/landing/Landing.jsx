1cc7cbc27fc371e81f43cf0e6c7ca0499e523951
IC <icbroqueza13@gmail.com>
2023-01-17 14:58:05 +0800

    feat: home page > features

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -3,6 +3,14 @@ import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 // import { headerImg } from 'src/assets/debitcard.png';
 
+import {
+  PermIdentity,
+  CreditScore,
+  ReceiptLong,
+  MailOutline,
+  Home,
+} from '@mui/icons-material';
+
 const Landing = () => {
   return (
     // <div className='w-full mx-auto px-10 py-10 mb-4 mt-5 bg-white shadow-md rounded'>
@@ -23,9 +31,9 @@ const Landing = () => {
     //     </button>
     //   </Link>
     // </div>
-    <div className='py-10 px-10'>
+    <div className='py-20 px-20'>
       {/* HERO SECTION */}
-      <section className='flex gap-5'>
+      <section className='flex gap-5 '>
         {/* HEADER */}
         <div className='flex flex-col w-1/2 border border-red-500 py-5 px-5 gap-5'>
           {/* TITLE */}
@@ -52,6 +60,63 @@ const Landing = () => {
         <div className=''>
           <img src='src/assets/debitcard.png' alt='' />
           image here
+        </div>
+      </section>
+
+      {/* FEATURED SECTION */}
+      <section>
+        <div className=''>
+          {/* FEATURED TITLE */}
+          <div className='flex flex-col my-20 py-10 w-1/2 mx-auto text-center'>
+            <h3 className='text-2xl font-bold mb-5'>
+              We Provide Quality Service
+            </h3>
+            <p className=''>
+              We will provide the best service to you so that you understand how
+              to use and get to know the features that we provide
+            </p>
+          </div>
+
+          {/* FEATURED ITEMS */}
+          <div className='flex gap-10 justify-center'>
+            {/* FEATURE 1 : LOAN MGT */}
+            <div className='flex flex-col w-80 p-10  border rounded-xl hover:shadow-xl hover:bg-red-500 hover:text-white cursor-pointer'>
+              <div className=''>
+                <CreditScore className=' w-full text-red-500 rounded-full h-full text-2xl mb-2 ' />
+              </div>
+              <h4 className='text-md font-semibold mb-1'>Loan Management</h4>
+              <p>
+                We will provide management for your loan so that you can track
+                them easily
+              </p>
+            </div>
+
+            {/* FEATURE 2 : CLIENT MGT */}
+            <div className='flex flex-col w-80 p-10 rounded-xl border hover:shadow-xl hover:bg-red-500 hover:text-white cursor-pointer'>
+              <div className=''>
+                <PermIdentity className=' w-full text-red-500 rounded-full h-full text-2xl mb-2' />
+              </div>
+              <h4 className='text-md font-semibold mb-1'>Client Management</h4>
+              <p>
+                We will provide management for your loan so that you can track
+                them easily
+              </p>
+            </div>
+
+            {/* FEATURE 3 : PAYMENT MGT */}
+            <div className='flex flex-col w-80 p-10 rounded-xl border hover:shadow-xl hover:bg-red-500 hover:text-white cursor-pointer '>
+              <div className=''>
+                <ReceiptLong className=' w-full text-red-500 rounded-full h-full text-2xl mb-2 ' />
+              </div>
+              <h4 className='text-md font-semibold mb-1'>
+                Payments Management
+              </h4>
+              <p>
+                We will provide management for your loan so that you can track
+                them easily
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
