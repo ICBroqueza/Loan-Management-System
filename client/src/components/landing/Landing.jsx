9dc0e6010790692e6eec3757e1d20df2a3c4e9a6
IC <icbroqueza13@gmail.com>
2023-01-17 23:05:39 +0800

    feat: landing page > contacts section

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -667,8 +667,67 @@ const Landing = () => {
         </div>
       </section>
 
-      {/* FAQ */}
-      <section></section>
+      {/* CONTACT */}
+      <section class=''>
+        <div class='py-8 lg:py-16 px-4 mx-auto max-w-screen-md'>
+          <h2 class='mb-4 text-3xl tracking-tight font-extrabold text-center text-gray-900 '>
+            Contact Us
+          </h2>
+          <p class='mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl'>
+            Got a technical issue? Want to send feedback about a beta feature?
+            Need details about our Business plan? Let us know.
+          </p>
+          <form action='#' class='space-y-8'>
+            <div>
+              <label
+                for='email'
+                class='block mb-2 text-sm font-medium text-gray-900'
+              >
+                Your email
+              </label>
+              <input
+                type='email'
+                id='email'
+                class='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5'
+                placeholder='name@flowbite.com'
+                required
+              />
+            </div>
+            <div>
+              <label
+                for='subject'
+                class='block mb-2 text-sm font-medium text-gray-900 '
+              >
+                Subject
+              </label>
+              <input
+                type='text'
+                id='subject'
+                class='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500'
+                placeholder='Let us know how we can help you'
+                required
+              />
+            </div>
+            <div class='sm:col-span-2'>
+              <label
+                for='message'
+                class='block mb-2 text-sm font-medium text-gray-900 '
+              >
+                Your message
+              </label>
+              <textarea
+                id='message'
+                rows='6'
+                class='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500'
+                placeholder='Leave a comment...'
+              ></textarea>
+            </div>
+            <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-5'>
+              Send
+            </button>
+          </form>
+        </div>
+      </section>
 
       {/* FOOTER */}
       <footer>
--- client/src/components/landing/Landing1.jsx
+++ client/src/components/landing/Landing1.jsx
@@ -1,13 +1,21 @@
-<p class="md:space-x-1 space-y-1 md:space-y-0 mb-4">
-  <a class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-    Link with href
-  </a>
-  <button class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-    Button with data-bs-target
-  </button>
-</p>
-<div class="collapse" id="collapseExample">
-  <div class="block p-6 rounded-lg shadow-lg bg-white">
-    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+<section class="bg-white dark:bg-gray-900">
+  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+      <form action="#" class="space-y-8">
+          <div>
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required>
+          </div>
+          <div>
+              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+              <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required>
+          </div>
+          <div class="sm:col-span-2">
+              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+              <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+          </div>
+          <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+      </form>
   </div>
-</div>
+</section>
