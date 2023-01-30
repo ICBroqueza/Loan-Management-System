179d66e0c31fe4901f42e6ae61a28d0479361c6b
IC <icbroqueza13@gmail.com>
2023-01-30 21:54:49 +0800

    fix: landing

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -43,8 +43,7 @@ const Landing = () => {
                   About
                 </a>
               </li>
-
-              {/* PRICING */}
+              {/* 
               <li>
                 <a
                   href='#pricing'
@@ -52,12 +51,12 @@ const Landing = () => {
                 >
                   Pricing
                 </a>
-              </li>
+              </li> */}
 
               {/* CONTACT */}
               <li>
                 <a
-                  href='#contact'
+                  href='#contacts'
                   class='block py-2 pl-3 pr-4   rounded text-gray-700    lg:hover:text-white lg:hover:bg-red-500 font-light lg:text-x '
                 >
                   Contact
@@ -86,7 +85,7 @@ const Landing = () => {
                 <Link to='/register'>Get Started</Link>
               </button>
               <button className='border border-gray-200 rounded sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100  font-bold py-2 px-4 focus:outline-none focus:shadow-outline mr-5'>
-                How it works
+                <a href='#contacts'>Contact Us</a>
               </button>
             </div>
           </div>
@@ -101,7 +100,7 @@ const Landing = () => {
       </section>
 
       {/* FEATURED SECTION */}
-      <section className='mb-20'>
+      <section id='featured' className='mb-20'>
         <div className='max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6'>
           {/* FEATURED TITLE */}
           <div className='flex flex-col my-20 py-10 w-1/2 mx-auto text-center'>
@@ -135,8 +134,8 @@ const Landing = () => {
               </div>
               <h4 className='text-md font-semibold mb-1'>Client Management</h4>
               <p>
-                We will provide management for your loan so that you can track
-                them easily
+                Offers client management plus managers can now email their
+                clients to notify them.
               </p>
             </div>
 
@@ -149,8 +148,8 @@ const Landing = () => {
                 Payments Management
               </h4>
               <p>
-                We will provide management for your loan so that you can track
-                them easily
+                Avoid payment delays by notifying managers about their client's
+                loan.
               </p>
             </div>
           </div>
@@ -356,9 +355,8 @@ const Landing = () => {
       </section>
 
       {/* PRICING SECTION */}
-      <section id='pricing'>
+      {/* <section id='pricing'>
         <div className='max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6'>
-          {/* TITLE */}
           <div class='max-w-screen-md mx-auto mb-8 text-center lg:mb-12'>
             <h2 class='mb-4 text-3xl font-extrabold tracking-tight text-gray-900 '>
               Designed for business teams like yours
@@ -369,9 +367,7 @@ const Landing = () => {
             </p>
           </div>
 
-          {/* PRICING ITEMS */}
           <div className='space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0'>
-            {/* PRICE 1 */}
             <div className='flex flex-col max-w-lg p-6 mx-auto text-center text-gray-900 bg-white border border-gray-200 rounded-lg shadow-md '>
               <h3 class='mb-4 text-2xl font-semibold'>Starter</h3>
               <p class='font-light text-gray-500 sm:text-lg dark:text-gray-400'>
@@ -456,7 +452,6 @@ const Landing = () => {
               </a>
             </div>
 
-            {/* PRICE 2 */}
             <div className='flex flex-col max-w-lg p-6 mx-auto text-center text-gray-900 bg-white border border-gray-200 rounded-lg shadow-md '>
               <h3 class='mb-4 text-2xl font-semibold'>Company</h3>
               <p class='font-light text-gray-500 sm:text-lg dark:text-gray-400'>
@@ -541,7 +536,6 @@ const Landing = () => {
               </a>
             </div>
 
-            {/* PRICE 3 */}
             <div className='flex flex-col max-w-lg p-6 mx-auto text-center text-gray-900 bg-white border border-gray-200 rounded-lg shadow-md '>
               <h3 class='mb-4 text-2xl font-semibold'>Enterprise</h3>
               <p class='font-light text-gray-500 sm:text-lg dark:text-gray-400'>
@@ -627,7 +621,7 @@ const Landing = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* BANNER 2 */}
       <section className='max-w-screen-xl px-4 mx-auto space-y-12 lg:space-y-20 lg:py-24 lg:px-6 bg-red-500 rounded-xl flex'>
@@ -647,7 +641,7 @@ const Landing = () => {
       </section>
 
       {/* CONTACT */}
-      <section id='contact'>
+      <section id='contacts'>
         <div class='py-8 lg:py-16 px-4 mx-auto max-w-screen-md'>
           <h2 class='mb-4 text-3xl tracking-tight font-extrabold text-center text-gray-900 '>
             Contact Us
@@ -711,139 +705,6 @@ const Landing = () => {
       {/* FOOTER */}
       <footer>
         <div class='max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10'>
-          <div class='grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5'>
-            <div>
-              <h3 class='mb-6 text-sm font-semibold text-gray-900 uppercase '>
-                Company
-              </h3>
-              <ul class='text-gray-500 '>
-                <li class='mb-4'>
-                  <a href='#' class=' hover:underline'>
-                    About
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Careers
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Brand Center
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Blog
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h3 class='mb-6 text-sm font-semibold text-gray-900 uppercase '>
-                Help center
-              </h3>
-              <ul class='text-gray-500 '>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Discord Server
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Twitter
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Facebook
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Contact Us
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h3 class='mb-6 text-sm font-semibold text-gray-900 uppercase '>
-                Legal
-              </h3>
-              <ul class='text-gray-500 '>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Privacy Policy
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Licensing
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Terms
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h3 class='mb-6 text-sm font-semibold text-gray-900 uppercase '>
-                Company
-              </h3>
-              <ul class='text-gray-500 '>
-                <li class='mb-4'>
-                  <a href='#' class=' hover:underline'>
-                    About
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Careers
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Brand Center
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Blog
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h3 class='mb-6 text-sm font-semibold text-gray-900 uppercase '>
-                Download
-              </h3>
-              <ul class='text-gray-500 '>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    iOS
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Android
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    Windows
-                  </a>
-                </li>
-                <li class='mb-4'>
-                  <a href='#' class='hover:underline'>
-                    MacOS
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
           <hr className='my-6 border-gray-200 sm:mx-auto  lg:my-8' />
           <div class='text-center'>
             <a
@@ -858,7 +719,7 @@ const Landing = () => {
             </span>
             <ul class='flex justify-center mt-5 space-x-5'>
               <li>
-                <a href='#' class='text-gray-500 hover:text-gray-900 '>
+                <a href='#' class='text-gray-500 hover:text-red-500 '>
                   <svg
                     class='w-5 h-5'
                     fill='currentColor'
@@ -874,7 +735,7 @@ const Landing = () => {
                 </a>
               </li>
               <li>
-                <a href='#' class='text-gray-500 hover:text-gray-900 '>
+                <a href='#' class='text-gray-500 hover:text-red-500  '>
                   <svg
                     class='w-5 h-5'
                     fill='currentColor'
@@ -890,7 +751,7 @@ const Landing = () => {
                 </a>
               </li>
               <li>
-                <a href='#' class='text-gray-500 hover:text-gray-900 '>
+                <a href='#' class='text-gray-500 hover:text-red-500  '>
                   <svg
                     class='w-5 h-5'
                     fill='currentColor'
@@ -902,7 +763,7 @@ const Landing = () => {
                 </a>
               </li>
               <li>
-                <a href='#' class='text-gray-500 hover:text-gray-900 '>
+                <a href='#' class='text-gray-500 hover:text-red-500  '>
                   <svg
                     class='w-5 h-5'
                     fill='currentColor'
@@ -912,22 +773,6 @@ const Landing = () => {
                     <path
                       fill-rule='evenodd'
                       d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z'
-                      clip-rule='evenodd'
-                    />
-                  </svg>
-                </a>
-              </li>
-              <li>
-                <a href='#' class='text-gray-500 hover:text-gray-900 '>
-                  <svg
-                    class='w-5 h-5'
-                    fill='currentColor'
-                    viewBox='0 0 24 24'
-                    aria-hidden='true'
-                  >
-                    <path
-                      fill-rule='evenodd'
-                      d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z'
                       clip-rule='evenodd'
                     />
                   </svg>
